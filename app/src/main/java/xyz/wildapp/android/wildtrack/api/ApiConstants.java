77eb3ee2f00cc85287fa3b7ed731d91f91cd2ec3
Vitalii Borisovskii <vborisovskii@mirantis.com>
2018-04-05 17:37:09 +0300

    Modify UI for track list

---
+++ app/src/main/java/xyz/wildapp/android/wildtrack/api/ApiConstants.java
@@ -0,0 +1,16 @@
+package xyz.wildapp.android.wildtrack.api;
+
+/**
+ * Created by vborisovskii on 4/5/18.
+ */
+
+public class ApiConstants {
+    public static final String STATUS_PENDING = "Pending";
+    public static final String STATUS_INFO_RECEIVED = "InfoReceived";
+    public static final String STATUS_IN_TRANSIT = "InTransit";
+    public static final String STATUS_OUT_FOR_DELIVERY = "OutForDelivery";
+    public static final String STATUS_FAIL = "AttemptFail";
+    public static final String STATUS_DELIVERED = "Delivered";
+    public static final String STATUS_ERROR = "Exception";
+    public static final String STATUS_EXPIRED = "Expired";
+}
